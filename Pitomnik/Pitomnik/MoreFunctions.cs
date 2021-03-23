7943b7a71741333acbfd3d2246a3d457f5527ade
dayofpay <42325421+dayofpay@users.noreply.github.com>
2021-03-23 05:20:13 +0200

    Add files via upload

---
+++ Pitomnik/Pitomnik/MoreFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Pitomnik
+{
+    class MoreFunctions
+    {
+        public static void Error()
+        {
+            Console.WriteLine("Неочаквана грешка ... връщаме ви в главното меню");
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+    }
+}
